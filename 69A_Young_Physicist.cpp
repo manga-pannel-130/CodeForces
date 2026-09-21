8a6da66759e24f455b72afa4dae6f2c41edeb675
Vishal Madhav <148483653+manga-pannel-130@users.noreply.github.com>
2026-09-21 15:30:16 +0530

    Sync Codeforces: 69A - Young Physicist [C++17 (GCC 7-32)]

---
+++ 69A_Young_Physicist.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    int xSum = 0, ySum = 0, zSum = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        int x, y, z;
+        cin >> x >> y >> z;
+ 
+        xSum += x;
+        ySum += y;
+        zSum += z;
+    }
+ 
+    if (xSum == 0 && ySum == 0 && zSum == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+ 
+    return 0;
+}
